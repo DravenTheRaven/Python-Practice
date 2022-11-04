381e1d19fed99f5e4de06d8e1a8724c88a15dc55
DravenTheRaven <106798464+DravenTheRaven@users.noreply.github.com>
2022-11-04 14:48:38 -0500

    Tkinter GUI Progress
    in tinker.py I copied code from
    https://gist.github.com/dat-adi/baf5a4a64358a5f1d13452ef7b584cbc
    
    I created app_skel.py to be exactly what its name implies. I'm going to implement more buttons and resize every thing. comments are a mess

--- app_skel.py
+++ app_skel.py
@@ -30,20 +30,45 @@ class WindowFrame(tk.Toplevel):
         self.three_x_label = ttk.Label(self, text="3XL Cost")
         self.three_x_label.grid(column=0, row=0)
 
-class QuoteFrame(ttk.Frame):
-    def __init__(self):
-        super().__init__()
-        # setup the grid layout manager
-        self.columnconfigure(0, weight=1)
-
+class ControlFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.title('ControlFrame')
+        self.geometry('400x150')
         self.pack()
+
         self.__create_widgets()
 
     def __create_widgets(self):
+        QuoteFrame.pack()
+        ResultFrame.pack()
+        switch_window_button = tk.Button(
+            self,
+            text="Go to the Side Page",
+            command=lambda: controller.show_frame(SidePage),
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
+
+
+
+class QuoteFrame(ttk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Quote Frame")
+        label.pack(padx=10, pady=10)
+
+
+        # setup the grid layout manager
+
+
+        self.pack()
+        self.__create_widgets(controller)
+
+    def __create_widgets(self, controller):
         self.customer_label = ttk.Label(self, text="Customer")
-        self.customer_label.grid(column=1, row=1, sticky=tk.W)
+        self.customer_label.pack()
         self.customer_entry = ttk.Entry(self, width=20)
-        self.customer_entry.grid(column=2, row=1, sticky=tk.W)
+        self.customer_entry.pack()
         self.customer_entry.focus()
 
 
@@ -51,15 +76,18 @@ class QuoteFrame(ttk.Frame):
 
         self.two_x_button = ttk.Button(self, text='2XL Cost')
         self.two_x_button['command']= self.open_two_x
-        self.two_x_button.grid(column=0, row=0)
+        self.two_x_button.pack()
 
         self.three_x_button = ttk.Button(self, text='3XL Cost')
         self.three_x_button['command']= self.open_three_x
-        self.three_x_button.grid(column=0, row=2)
+        self.three_x_button.pack()
 
-        self.change_button = ttk.Button(self, text='Change')
-        self.change_button['command']=self.__change_frames(ResultFrame)
-        self.change_button.grid(column=0, row=3)
+        switch_window_button = tk.Button(
+            self,
+            text="Generate Quote",
+            command=lambda: controller.show_frame(ResultFrame),
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
 
     def open_two_x(self):
         window_two = Window(self)
@@ -69,59 +97,72 @@ class QuoteFrame(ttk.Frame):
         window_three = WindowFrame(self)
         window_three.grab_set()
 
-    def __change_frames(self, ResultFrame):
-
-        ResultFrame.__create_widgets()
-        ResultFrame.grid(self)
-        self.grid_remove()
 
 
-class ResultFrame(ttk.Frame):
-    def __init__(self, QuoteFrame):
-        super().__init__(QuoteFrame)
+
+class ResultFrame(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Main Page")
+        label.pack(padx=10, pady=10)
         # setup the grid layout manager
-        self.columnconfigure(0, weight=1)
 
+        self.switch_window_button = tk.Button(self, text='Change', command=lambda: controller.show_frame(CompletionScreen))
+        self.switch_window_button.pack(side="bottom", fill=tk.X)
         self.__create_widgets()
 
 
     def __create_widgets(self):
         self.customer_label = ttk.Label(self, text="Customer")
-        self.customer_label.grid(column=1, row=1, sticky=tk.W)
+        self.customer_label.pack(side="left")
         self.customer_entry = ttk.Entry(self, width=20)
-        self.customer_entry.grid(column=2, row=1, sticky=tk.W)
+        self.customer_entry.pack(side="left")
         self.customer_entry.focus()
 
-        self.change_button = ttk.Button(self, text='Change')
-        self.change_button['command']=self.__change_frames
-        self.change_button.grid(column=0, row=3)
-
-    def __change_frames(QuoteFrame):
-        QuoteFrame.pack()
 
 
+
+class CompletionScreen(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Complete")
+        label.pack(padx=10, pady=10)
+        switch_window_button = ttk.Button(
+            self, text="Return to menu", command=lambda: controller.show_frame(QuoteFrame)
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
 
 class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
         self.title('Quote Calculator')
-        self.geometry('400x150')
-        self.resizable(0, 0)
-        # windows only (remove the minimize/maximize button)
-        self.attributes('-toolwindow', True)
+        container = tk.Frame(self, height=400, width=600)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (QuoteFrame, ResultFrame, CompletionScreen):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(QuoteFrame)
+
+    def show_frame(self, cont):
+        frame=self.frames[cont]
+
+        frame.tkraise()
+
 
         # layout on the root window
-
-
-    def open_window(self):
-        window = ResultFrame(self)
-        window.pack(self)
 
 
 
 if __name__ == "__main__":
     app = App()
-
-    quote_frame = QuoteFrame(app)
-
     app.mainloop()
--- tinker.py
+++ tinker.py
@@ -1,42 +1,81 @@
-# Import Module
-from tkinter import *
-
-# create root window
-root = Tk()
-
-# root window title and dimension
-root.title("Welcome to GeekForGeeks")
-# Set geometry(widthxheight)
-root.geometry('350x200')
-
-menu = Menu(root)
-item = Menu(menu)
-item.add_command(label = 'New')
-menu.add_cascade(label='File', menu=item)
-root.config(menu=menu)
+import tkinter as tk
+from tkinter import ttk
 
 
-# adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
+class windows(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        # Adding a title to the window
+        self.wm_title("Test Application")
 
-# adding Entry Field
-txt = Entry(root, width=10)
-txt.grid(column =1, row =0)
+        # creating a frame and assigning it to container
+        container = tk.Frame(self, height=400, width=600)
+        # specifying the region where the frame is packed in root
+        container.pack(side="top", fill="both", expand=True)
+
+        # configuring the location of the container using grid
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # We will now create a dictionary of frames
+        self.frames = {}
+        # we'll create the frames themselves later but let's add the components to the dictionary.
+        for F in (MainPage, SidePage, CompletionScreen):
+            frame = F(container, self)
+
+            # the windows class acts as the root window for the frames.
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # Using a method to switch frames
+        self.show_frame(MainPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        # raises the current frame to the top
+        frame.tkraise()
 
 
-# function to display user text when
-# button is clicked
-def clicked():
+class MainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Main Page")
+        label.pack(padx=10, pady=10)
 
-    res = "You wrote" + txt.get()
-    lbl.configure(text = res)
+        # We use the switch_window_button in order to call the show_frame() method as a lambda function
+        switch_window_button = tk.Button(
+            self,
+            text="Go to the Side Page",
+            command=lambda: controller.show_frame(SidePage),
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
 
-# button widget with red color text inside
-btn = Button(root, text = "Click me" ,
-             fg = "red", command=clicked)
-# Set Button Grid
-btn.grid(column=2, row=0)
 
-# Execute Tkinter
-root.mainloop()
+class SidePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="This is the Side Page")
+        label.pack(padx=10, pady=10)
+
+        switch_window_button = tk.Button(
+            self,
+            text="Go to the Completion Screen",
+            command=lambda: controller.show_frame(CompletionScreen),
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
+
+
+class CompletionScreen(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Completion Screen, we did it!")
+        label.pack(padx=10, pady=10)
+        switch_window_button = ttk.Button(
+            self, text="Return to menu", command=lambda: controller.show_frame(MainPage)
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
+
+
+if __name__ == "__main__":
+    testObj = windows()
+    testObj.mainloop()
